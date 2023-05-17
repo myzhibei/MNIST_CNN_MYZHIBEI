3937f8609ec20ca96e5524ca6b3bb67fe9e8a8e6
myzhibei <myzhibei@qq.com>
2023-05-18 00:36:44 +0800

    add CNN

--- mnist/MNIST_CNN.py
+++ mnist/MNIST_CNN.py
@@ -2,12 +2,14 @@
 Author: myzhibei myzhibei@qq.com
 Date: 2023-05-17 21:15:42
 LastEditors: myzhibei myzhibei@qq.com
-LastEditTime: 2023-05-17 23:18:22
+LastEditTime: 2023-05-18 00:27:18
 FilePath: \手写数字识别\mnist\MNIST_CNN.py
 Description: 
 
 Copyright (c) 2023 by myzhibei myzhibei@qq.com, All Rights Reserved. 
 '''
+import sys
+import time
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -18,6 +20,11 @@ from torchvision.transforms import ToTensor
 log_path = r"./logs"  # 日志文件路径
 dataset_path = r"./data"  # 数据集存放路径
 model_save_path = r"./model"  # 模型待存储路径
+
+logf = open(log_path+'/'+'runCNN.log', 'w')
+sys.stdout = logf
+
+CNN_bool = True
 
 # 下载数据集
 # Download training data from open datasets.
@@ -58,9 +65,8 @@ device = (
 )
 print(f"Using {device} device")
 
+
 # Define model
-
-
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
@@ -82,8 +88,40 @@ class NeuralNetwork(nn.Module):
 model = NeuralNetwork().to(device)
 print(model)
 
+
+class Lambda(nn.Module):
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
+
+    def forward(self, x):
+        return self.func(x)
+
+
+def preprocess(x):
+    return x.view(-1, 1, 28, 28)
+
+
+CNN_model = nn.Sequential(
+    Lambda(preprocess),
+    nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1),
+    nn.ReLU(),
+    nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1),
+    nn.ReLU(),
+    nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1),
+    nn.ReLU(),
+    nn.AvgPool2d(4),
+    Lambda(lambda x: x.view(x.size(0), -1)),
+)
+
+CNN_model.to(device)
+print(CNN_model)
+
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+if CNN_bool:
+    optimizer = torch.optim.SGD(CNN_model.parameters(), lr=1e-3, momentum=0.9)
+else:
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
 
 
 def train(dataloader, model, loss_fn, optimizer):
@@ -123,40 +161,92 @@ def test(dataloader, model, loss_fn):
         f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
+epochs = 10
+
+starttime = time.time()
+if CNN_bool:
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, CNN_model, loss_fn, optimizer)
+        test(test_dataloader, CNN_model, loss_fn)
+else:
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer)
+        test(test_dataloader, model, loss_fn)
+endtime = time.time()
 print("Done!")
+print(f"Time-consuming: {(endtime - starttime)} \n")
+
+if CNN_bool:
+    model_name = "MNIST_CNN_model.pth"
+    model_path = model_save_path + '/' + model_name
+    torch.save(CNN_model.state_dict(), model_path)
+    print("Saved PyTorch CNN Model State to {model_path}")
+
+    CNN_model_2 = nn.Sequential(
+        Lambda(preprocess),
+        nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1),
+        nn.ReLU(),
+        nn.AvgPool2d(4),
+        Lambda(lambda x: x.view(x.size(0), -1)),
+    )
+    CNN_model_2.load_state_dict(torch.load(model_path))
+
+    classes = [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+    ]
+
+    CNN_model_2.eval()
+    x, y = test_data[0][0], test_data[0][1]
+    with torch.no_grad():
+        x = x.to(device)
+        pred = CNN_model_2(x)
+        predicted, actual = classes[pred[0].argmax(0)], classes[y]
+        print(f'Predicted: "{predicted}", Actual: "{actual}"')
+
+else:
+    model_name = "MNIST_model.pth"
+    model_path = model_save_path + '/' + model_name
+
+    torch.save(model.state_dict(), model_path)
+    print("Saved PyTorch Model State to {model_path}")
+
+    model = NeuralNetwork().to(device)
+    model.load_state_dict(torch.load(model_path))
+
+    classes = [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+    ]
+    model.eval()
+    x, y = test_data[0][0], test_data[0][1]
+    with torch.no_grad():
+        x = x.to(device)
+        pred = model(x)
+        predicted, actual = classes[pred[0].argmax(0)], classes[y]
+        print(f'Predicted: "{predicted}", Actual: "{actual}"')
 
 
-model_name = "MNIST_CNN_model.pth"  # 模型名字
-model_path = model_save_path + '/' + model_name
-
-torch.save(model.state_dict(), model_path)
-print("Saved PyTorch Model State to {model_path}")
-
-model = NeuralNetwork().to(device)
-model.load_state_dict(torch.load(model_path))
-
-classes = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-]
-
-model.eval()
-x, y = test_data[0][0], test_data[0][1]
-with torch.no_grad():
-    x = x.to(device)
-    pred = model(x)
-    predicted, actual = classes[pred[0].argmax(0)], classes[y]
-    print(f'Predicted: "{predicted}", Actual: "{actual}"')
+logf.close()
